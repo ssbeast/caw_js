02f121abd3d3b0ee5678d85898fc71e3215fc2ba
ssbeast <ssbeast007@gmail.com>
2022-06-16 16:43:05 +0530

    functionality completed

--- pomodoroTimer/main.js
+++ pomodoroTimer/main.js
@@ -1,14 +1,15 @@
+const ringColor = document.querySelector('.ring');
 const minutes = document.querySelector('#minutes__input');
 const seconds = document.querySelector('#seconds__input');
 const startButton = document.querySelector('.start');
 const settingsButton = document.querySelector('.settings');
 
 var timer;
-var startFlag = true;
+var isStartValid = true;
 
 const checkValidTime = (minutes, seconds) => {
 
-    if (!(minutes >= 0 && minutes < 60) || !(seconds >= 0 && seconds < 60) || minutes == "" || seconds == "") {
+    if (!(minutes >= 0 && minutes < 60) || !(seconds >= 0 && seconds < 60) || minutes.trim() == "" || seconds.trim() == "") {
 
         return false;
     } else {
@@ -19,12 +20,17 @@ const checkValidTime = (minutes, seconds) => {
 
 startButton.addEventListener('click', () => {
 
+    minutes.disabled = true;
+    seconds.disabled = true;
+
     if (checkValidTime(minutes.value, seconds.value)) {
 
         startTimer();
     } else {
 
         alert("Dear user, Please enter valid time ; )");
+        minutes.value = "00";
+        seconds.value = "15";
     }
 })
 
@@ -37,25 +43,40 @@ settingsButton.addEventListener('click', () => {
 
 const startTimer = () => {
 
-    if (startFlag == true) {
+    if (isStartValid == true) {
 
         startButton.innerHTML = "Stop";
-        startFlag = false;
+        isStartValid = false;
 
         timer = setInterval(() => {
-        
-        if (seconds.value == 0) {
 
-            minutes.value -= 1;
-            seconds.value = 59;
-        } else {
+            if (minutes.value == 0 && seconds.value == 0) {
 
-            seconds.value -= 1;
-        }
-    }, 1000);
+                stopTimer();
+                ringColor.style.stroke = "#900A0A";
+
+                setTimeout(() => {
+
+                    alert("GAME OVER! Time's UP ; )");
+                    ringColor.style.stroke = "#09A65A";
+                }, 100)
+            } else {
+
+                if (seconds.value == 0) {
+                    
+                    minutes.value -= 1;
+                    minutes.value = ('0' + minutes.value).slice(-2);
+                    seconds.value = 59;
+                } else {
+
+                    seconds.value -= 1;
+                    seconds.value = ('0' + seconds.value).slice(-2);
+                }
+            } 
+        }, 1000);
     } else {
 
-        startFlag = true;
+        isStartValid = true;
         stopTimer();
     }
 }
@@ -65,4 +86,3 @@ const stopTimer = () => {
     startButton.innerHTML = "Start";
     clearInterval(timer);
 }
-
